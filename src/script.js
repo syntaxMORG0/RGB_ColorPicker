fa1ec1662ef2e79b8373133532d3684fa91cb385
syntaxMORG0 <129966083+syntaxMORG0@users.noreply.github.com>
2023-04-13 11:56:38 +0000

    Added a convert to ColorHex

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ const redInput = document.querySelector('input[placeholder="R"]');
 const greenInput = document.querySelector('input[placeholder="G"]');
 const blueInput = document.querySelector('input[placeholder="B"]');
 const finalColorDiv = document.querySelector('.final-color');
+const saveAsHexButton = document.querySelector('#SaveAsHex');
 
 redInput.addEventListener('input', updateFinalColor);
 greenInput.addEventListener('input', updateFinalColor);
@@ -13,6 +14,17 @@ function updateFinalColor() {
   const blueValue = blueInput.value;
   const rgbValue = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
   finalColorDiv.style.backgroundColor = rgbValue;
+  const hexValue = rgbToHex(redValue, greenValue, blueValue);
+  saveAsHexButton.textContent = `Save Hex (${hexValue})`;
+}
+
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
 const Profile = document.getElementById("Profile");
@@ -23,5 +35,14 @@ Profile.addEventListener("click", function() {
 const Source = document.getElementById("Source");
 Source.addEventListener("click", function() {
   window.location.href = "https://github.com/syntaxMORG0/RGB_ColorPicker";
+});
+
+saveAsHexButton.addEventListener("click", function() {
+  const redValue = redInput.value;
+  const greenValue = greenInput.value;
+  const blueValue = blueInput.value;
+  const hexValue = rgbToHex(redValue, greenValue, blueValue);
+  alert(`Hex code: (${hexValue}) You can copy it now =)`);
+  console.log(`A user now have generated a color HEX-code: ${hexValue}`)
 });
 
